74ef92f587b8a2b27c48da6d1782ce490a18b94d
Sameer <123326498+SInsanali@users.noreply.github.com>
2024-05-20 08:46:28 -0600

    Update pizzaorder.py

--- Day 3/pizzaorder.py
+++ Day 3/pizzaorder.py
@@ -2,11 +2,9 @@
 
 
 print("Thank you for choosing Python Pizza Deliveries!")
-size = input() # What size pizza do you want? S, M, or L
-add_pepperoni = input() # Do you want pepperoni? Y or N
-extra_cheese = input() # Do you want extra cheese? Y or N
-# 🚨 Don't change the code above 👆
-# Write your code below this line 👇
+size = input("What size pizza do you want? S, M, or L\n")
+add_pepperoni = input("Do you want pepperoni? Y or N\n")
+extra_cheese = input("Do you want extra cheese? Y or N\n")
 
 #create bill variable
 bill = int(0)
