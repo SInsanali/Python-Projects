f02db6e7282c8acdd4023823b51ddef3ccf80b1b
Sam Insanali <sameer.insanali@gmail.com>
2024-06-05 08:44:06 -0600

    Improved shift cypher program

--- Day_8/shift_cypher/main.py
+++ Day_8/shift_cypher/main.py
@@ -1,62 +1,79 @@
-# Tons of pseudo code in this to help me understand the logic as I code :)
+import art
 
+# Tons of pseudo code in this to help me understand the logic as I code :)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # Define functions
-def encode(plain_text, shift_value):
-    cypher_text = ""
-    for char in plain_text:
+def logic(instruction, input_text, shift_value):
+    output_text = ""
+   
+    # Used for later functionality in a print statement
+    if instruction == "e":
+        operation = "encoded"
+    elif instruction == "d":
+        operation = "decoded"
+
+    for char in input_text:
         # Checks if the character is a letter in list "alphabet"
         if char in alphabet:
             # Find index number for the character
             char_og_position = alphabet.index(char)
-            # Add the shift while the character is represented as an index value
-            char_new_position = (char_og_position + shift_value) % 26
-            # Convert back to a letter
+            # Add or subtract the shift value while the character is represented as an index value
+            if instruction == "e":
+                char_new_position = (char_og_position + shift_value) % 26
+            elif instruction == "d":
+                char_new_position = (char_og_position - shift_value) % 26
+            # Convert back to a letter        
             new_char = alphabet[char_new_position]
             # Append to string
-            cypher_text += new_char
-        # If not in alphabet, append to string 
+            output_text += new_char
+        # If not in alphabet, append to string
         else:
-            cypher_text += char
-    
-    # Print output
-    print(f"The encoded text is: {cypher_text}")
-
-def decode(encoded_text, shift_value):
-    decoded_text = ""
-    for char in encoded_text:
-        if char in alphabet:
-            char_og_position = alphabet.index(char)
-            char_new_position = (char_og_position - shift_value) % 26
-            new_char = alphabet[char_new_position]
-            decoded_text += new_char
-        else:
-            decoded_text += char
+            output_text += char
             
-    print(f"The decoded text is: {decoded_text}")
+    print(f"\nYour {operation} text is: {output_text}")
 
+# Need to update this to made the program a while loop so it can handle input errors and continue on. 
 
-#need to update this to made the program a while loop so it can handle input errors and continue on. 
+print(art.banner)
 
-# Gather user inputs and output conditional logic
-direction = input('Type "e" to encode, type "d" to decode:\n')
+run = True
 
-#input validation
-valid_inputs = ["e", "d"]
+while run:
+    # Gather user inputs and output conditional logic
+    direction = input('\nType "e" to encode, type "d" to decode:\n')
+    #input validation
+    if direction != "e" and direction != "d":
+        print("Please enter a valid single letter.")
+        continue
 
-
-
-if direction not in valid_inputs:
-    print("Please choose a valid entry")
-
-else:
     text = input("Enter your message to be converted:\n").lower()
-    shift = int(input("How many places will this be shifted?:\n"))
+    
+    shift_input = input("How many places will this be shifted?:\n")
+    # Input validation for shift
+    
+    
+    #need to work on this
+    if not shift_input.isdigit():
+        print("Please enter a valid number.")
+        continue
+    shift = int(shift_input)
+    #need to work on this
+    
 
-if direction == "e":
-    encode(text, shift)
-elif direction == "d":
-    decode(text, shift)
 
+    #execute logic
+    logic(direction, text, shift)
 
+    #run or end program
+    go_again = input('\nWould you like to run the program again? Type "y" to run again or "n" to end the program:\n')
+
+    # Asks the user if they want to go again and adds input validation.
+    if go_again != "y" and go_again != "n":
+        print("Please enter a valid single letter.")
+        continue
+    elif go_again == "y":
+        continue
+    elif go_again == "n":
+        run = False
+        print("Thank you for using this program!")
