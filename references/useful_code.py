f830cdcf26fb46f510d09b09b217e88ea0fe93ff
SInsanali <sameer.insanali@gmail.com>
2024-06-20 20:21:27 -0600

    created functions module

--- references/useful_code.py
+++ references/useful_code.py
@@ -32,32 +32,47 @@ try:
     #Wrap the main code in here
 except KeyboardInterrupt:
     #Print an error message and exit cleanly
-    print("\nExecution interrupted by user. Exiting program cleanly.")
+    print("\nExecution interrupted by user. Exiting program.")
 
 #############################################################################################
 
 #NOTE Input validation functions
 
-def validate_integer(input_str):
-    """Validates that the input string is an integer."""
+#When the return statement is executed in a function,
+#it returns the value to the caller of the function. 
+#The caller can then assign this returned value to a variable.
+
+def validate_integer(prompt_message):
+    """Validates user input is an integer.
+    ex. user_input = validate_integer("Enter a value: ")"""
     while True:
+        input_str = input(prompt_message)
         try:
             return int(input_str)
         except ValueError:
-            input_str = input("Invalid input. Please enter a valid number: ")
+            print("Invalid input. Please enter a valid number.")
 
-def validate_float(input_str):
-    """Validates that the input string is a float."""
+
+
+
+def validate_float(prompt_message):
+    """Validates user input is a decimal.
+    ex. user_input = validate_float("Enter a value: ")"""
     while True:
+        input_str = input(prompt_message)
         try:
-            return float(input_str)
+            return float(prompt_message)
         except ValueError:
-            input_str = input("Invalid input. Please enter a valid number: ")
+            print("Invalid input. Please enter a valid number")
 
-def validate_non_empty_string(input_str):
-    """Validates that the input string is non-empty."""
-    while not input_str.strip(): #Remove spaces at the beginning and at the end of the string
-        input_str = input("Invalid input. Please enter a non-empty string: ")
-    return input_str.strip()
+
+
+
+#FIXME
+# def validate_non_empty_string(input_str):
+#     """Validates that the input string is non-empty."""
+#     while not input_str.strip(): #Remove spaces at the beginning and at the end of the string
+#         input_str = input("Invalid input. Please enter a non-empty string: ")
+#     return input_str.strip()
 
 #############################################################################################
