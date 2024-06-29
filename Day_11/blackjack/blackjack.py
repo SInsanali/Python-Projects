58abc2227a4e6785ca8c1781cc996e2333a4ce11
SInsanali <sameer.insanali@gmail.com>
2024-06-29 02:02:19 -0600

    Finished blackjack project

--- Day_11/blackjack/blackjack.py
+++ Day_11/blackjack/blackjack.py
@@ -5,59 +5,73 @@
 # Description:
 # This program is an interactive blackjack game
 # The goal is to test all of the skills I've leaned up to this point.
+
+# retrospectively, I have no idea why I used so many functions and made it so recursive
+# in the end it made the code much harder to debug
+# lesson learned lol
 ##################################################################################################
 from art import banner
 import random
 
-#cards in deck and players hands
+# cards in deck and players hands
 deck = [11, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 players_cards = []
 dealers_cards = []
 
-#clear screen function
+# clear screen function
 from os import system, name
-def clear(): 
+
+
+def clear():
     """Clears the screen on the console"""
-    #for windows
+    # for windows
     if name == "nt":
         system("cls")
-    #for mac and linux(here, os.name is 'posix')
+    # for mac and linux(here, os.name is 'posix')
     else:
         system("clear")
 
+
 def calculate_score(hand):
-    """Caculates the total value of a hand"""
-    score = 0
-    for card in hand:
-        score += card
+    """Calculates the total value of a hand"""
+    score = sum(hand)
+    ace_count = hand.count(11)
+    while score > 21 and ace_count:
+        ace_count -= 1
+        score -= 10
     return score
 
-def display_hands(show_dealer_hand = False):
+
+def display_hands(show_dealer_hand=False):
     """Displays the players and dealers hand"""
     player_score = calculate_score(players_cards)
     dealers_score = calculate_score(dealers_cards)
-    
-    print(f"Players hand: {players_cards}   Total hand value: {player_score}") #prints both of the players cards
-   
-    if show_dealer_hand:
+
+    print(f"Players hand: {players_cards}   Total hand value: {player_score}")  # prints both of the players cards
+
+    if show_dealer_hand:  # when game is over
         print(f"Dealers hand: {dealers_cards}   Total hand value: {dealers_score}")
-    else:
+    else:  # when game is not over
         print(f"Dealers hand [{dealers_cards[0]}, ?]")
+
 
 def first_hand():
     """Used to deal the initial hand for the game"""
     clear()
-    print(banner) #blackjack banner
-    while len(players_cards) != 2: #deals 2 cards
+    print(banner)  # blackjack banner
+    players_cards.clear()
+    dealers_cards.clear()
+
+    while len(players_cards) != 2:  # deals 2 cards for player
         random_card_choice = random.choice(deck)
-        players_cards.append(random_card_choice) #for player
-        
-        random_card_choice = random.choice(deck)
-        dealers_cards.append(random_card_choice) #for dealer
+        players_cards.append(random_card_choice)
+
+    while calculate_score(dealers_cards) < 17:
+        dealers_cards.append(random.choice(deck))
+
     display_hands()
 
 
-#user input determines the next step in the game
 def user_prompt():
     """Determines if the game continues on"""
     while True:
@@ -70,48 +84,73 @@ def user_prompt():
         else:
             print("Please choose a valid option")
 
-def deal_card(hand):
-    if hand == "player":
-        players_cards.append(random.choice(deck))
-    
-    while calculate_score(dealers_cards) < 16:
-        dealers_cards.append(random.choice(deck))
 
-        if calculate_score(dealers_cards) <= 17:
-            break # NOTE is break used correctly here?
+def deal_card():
+    players_cards.append(random.choice(deck))
+    display_hands(show_dealer_hand=False)
 
 
+def final_display():
+    player_score = calculate_score(players_cards)
+    dealers_score = calculate_score(dealers_cards)
+
+    if player_score == 21:
+        print("\nBlackjack! You win.")
+    elif dealers_score == 21:
+        print("\nThe dealer has blackjack, you lose.")
+
+    if dealers_score > 21:
+        print("\nThe dealer busted! You win!")
+    elif player_score > 21:
+        print("\nYou've busted!")
+
+    if player_score == dealers_score:
+        print("\nIt's a tie.")
+    elif player_score > dealers_score and player_score <= 21:
+        print("\nYou win!")
+    else:
+        print("\nThe dealer wins.")
 
 
-#Used all times after the first hand
-def deal_hand():
-    """Deals a card to both players"""
-    should_continue = user_prompt() #recursively calls this
-    if should_continue == "1":
-       deal_a_card()
-       display_hands()
+def play_again_prompt():
+    while True:
+        valid_choices = ["1", "2"]
+        play_again = input("Type '1' to play again\
+                            \nType '2' to exit\n")
+        if play_again in valid_choices:
+            return play_again
+        else:
+            print("Please choose a valid choice")
 
 
+def game():
+    while True:
+        first_hand()  # deal the first hand
+
+        game_running = True
+        while game_running:
+            should_continue = user_prompt()
+            if should_continue == "1":
+                deal_card()
+            elif should_continue == "2":
+                game_running = False
+
+        # Dealer's turn
+        while calculate_score(dealers_cards) < 17:
+            dealers_cards.append(random.choice(deck))
+
+        display_hands(show_dealer_hand=True)  # Display final hands after dealer's turn
+        final_display()
+
+        play_again = play_again_prompt()
+        if play_again == "2":
+            print("\n\nThanks for checking out the program.\nHave a great day!")
+            break
 
 
-#testing
-first_hand()
-deal_hand()
-print(dealers_cards)
-
-
-#TODO make/modify function to be called when the game is over to show both players cards and totals. 
-#TODO add rule for calculating ace as a 1 if total is over 21
-#TODO add an input for calculate players score() maybe???
-
-
-#NOTE Dealer must hit if their hand total is 16 or less.
-#NOTE Dealer must stand if their hand total is 17 or more.
-
-#  def display_hands(show_all_dealer_cards=False):
-#     """Displays the player's and dealer's hands"""
-#     if show_all_dealer_cards:
-#         print(f"Dealer's hand: {dealers_cards} Total hand value: {calculate_score(dealers_cards)}")
-#     else:
-#         print(f"Dealer's hand: [{dealers_cards[0]}, ?]")
-#     print(f"Player's hand: {players_cards} Total hand value: {calculate_score(players_cards)}")
+# This is it I guess...
+try:
+    game()
+except KeyboardInterrupt:
+    # Print an error message and exit cleanly
+    print("\nExecution interrupted by user. Exiting program.")
