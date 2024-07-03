8bb40f5d682d60948e164a84e30fa1dc402a7759
Sam Insanali <sameer.insanali@gmail.com>
2024-07-03 13:00:35 -0600

    Added play again functiona

--- Day_12/guess_the_number/guess_the_number.py
+++ Day_12/guess_the_number/guess_the_number.py
@@ -7,7 +7,6 @@
 #The goal is to practice defining the scope of the code that I write
 #emphasis on using/modifying global and local variables.
 
-#I could do what I usually do and add input validation for everything
 ##################################################################################################
 from art import logo
 import random
@@ -23,6 +22,16 @@ def clear():
     else:
         system("clear")
 
+def validate_integer(prompt_message):
+    """Validates user input is an integer.
+    ex. user_input = validate_integer("Enter a value: ")"""
+    while True:
+        input_str = input(prompt_message)
+        try:
+            return int(input_str)
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
 def decrease_lives():
     global player_lives
     return player_lives - 1
@@ -36,37 +45,64 @@ def player_hint():
 
 
 #####program#####
-try:   
-    clear()
-    print(logo)
+try:
+    program_running = True
 
-    RANDOM_NUMBER = random.randint(1,100) #random number picked from computer #this is a global constant
+    while program_running:
+        clear()
+        print(logo)
 
-    print("[-] I'm thinking of a number between 1 and 100")
-    game_difficulty = input('[?] Choose a difficulty, type "easy" or "hard" : ') # i'll add input validation later if i feel like it. 
+        RANDOM_NUMBER = random.randint(1,100) #random number picked from computer #this is a global constant
 
-    #set player lives based on the difficulty chosen
-    if game_difficulty == "easy":
-        player_lives = 10
-    elif game_difficulty == "hard":
-        player_lives = 5
+        print("[-] I'm thinking of a number between 1 and 100")
 
-
-    print(RANDOM_NUMBER) #testing
-
-    while player_lives != 0:
-        print(f"[-] You have {player_lives} guesses left")
-        guess = int(input("\n[?] Guess a number: "))
-
-        if guess == RANDOM_NUMBER:
-            print(f"[-] You got it! The answer was {RANDOM_NUMBER}")
-            break
-        else: 
-            player_lives = decrease_lives()
-            player_hint()
+        while True:
+            game_difficulty = input('[?] Choose a difficulty, type "easy" or "hard" : ').lower()
+            valid_options = ["easy", "hard"]
+            if game_difficulty in valid_options:
+                break
+            else:
+                print("[!] Please enter a valid input.")
             
-    if player_lives == 0:
-        print(f"[-] You ran out of guesses, the answer was {RANDOM_NUMBER}")
+        #set player lives based on the difficulty chosen
+        if game_difficulty == "easy":
+            player_lives = 10
+        elif game_difficulty == "hard":
+            player_lives = 5
+
+        while player_lives != 0:
+            print(f"[-] You have {player_lives} guesses left")
+            
+            guess = validate_integer("\n[?] Guess a number: ")
+
+            if guess == RANDOM_NUMBER:
+                print(f"[-] You got it! The answer was {RANDOM_NUMBER}")
+                break
+            else: 
+                player_lives = decrease_lives()
+                player_hint()
+                
+        if player_lives == 0:
+            print(f"[-] You ran out of guesses, the answer was {RANDOM_NUMBER}")
+            
         
+            #run program again? 
+        while True:
+            valid_input = ["1", "2"]
+            play_again = input("\n[?] Type '1' to play again | Type '2' to end the program: ")
+
+            if play_again in valid_input:
+                break
+            else:
+                print("[!] Please choose a valid option")
+
+
+            #run program again logic
+        if play_again == "1":
+            continue #restart the loop for another conversion
+        else:
+            print("[-] Thank you for using this program")
+            program_running = False #exit the loop and end the program
+
 except KeyboardInterrupt:
     print("\nExecution interrupted by user. Exiting program.")
